2fe63f4298aa9e970f11f1ed8f9d3610cc81ce4c
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2021-12-28 16:16:42 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,6 @@
---2021-12-25 15:03:19--  https://raw.githubusercontent.com/unx21/ayame/zx/handler.js
-Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.109.133, 185.199.110.133, 185.199.111.133, ...
-Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.109.133|:443... connected.
-HTTP request sent, awaiting response... 200 OK
-Length: 22374 (22K) [text/plain]
-Saving to: ‘handler.js.1’
-
-     0K .......... .......... .                               100% 14.6M=0.001s
-
-2021-12-25 15:03:19 (14.6 MB/s) - ‘handler.js.1’ saved [22374/22374]
-
+git init
+git add *.json
+git config --global user.name "FIRST_NAME LAST_NAME"
+git config --global user.email "MY_NAME@example.com"
+git commit -m herok
+git push --force --set-upstream https://rishabh-modi2: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@github.com/rishabh-modi2/unsudo-apt
