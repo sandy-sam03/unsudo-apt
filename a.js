71a14febf6a908476ec8cbeefd8da771bb99dfe4
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2021-12-29 13:12:52 +0530

    Update a.js

--- a.js
+++ a.js
@@ -3,4 +3,4 @@ git add *.json
 git config --global user.name "FIRST_NAME LAST_NAME"
 git config --global user.email "MY_NAME@example.com"
 git commit -m herok
-git push --force --set-upstream https://rishabh-modi2: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@github.com/rishabh-modi2/unsudo-apt
+git push --force --set-upstream https://rishabh-modi2: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@github.com/rishabh-modi2/unsudo-apt master
