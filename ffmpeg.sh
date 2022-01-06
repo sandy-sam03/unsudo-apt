080fbaf9e6630f3540feceadf6adbd51ffaf4395
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-01-06 09:36:32 +0530

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,1 +1,1 @@
-wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz && tar -xf *.xz && rm *.xz && cd ffmpeg-git-amd64-static && mv * /layers/heroku_nodejs-engine/nodejs/bin
+wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz && tar -xf *.xz && rm *.xz && cd ffmpe* && mv * /layers/heroku_nodejs-engine/nodejs/bin
