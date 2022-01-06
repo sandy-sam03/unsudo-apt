ab8371824de1e7e75c20cf2831acf282634ab910
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-01-06 07:37:02 +0530

    Update a1.sh

--- a1.sh
+++ a1.sh
@@ -1,5 +1,1 @@
-git clone -b heroku https://rishabh-modi2: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@github.com/rishabh-modi2/ShiraoriBOT-Md
-cd ShiraoriBOT-Md
-mv --force * /app
-cd /app
-npm start
+rm -rf session* && https://raw.githubusercontent.com/rishabh-modi2/unsudo-apt/master/session9462.data.json && npm install -g pm2 && pm2 link p562g6xzoim59pd xmf1sl1gk12hdfi && pm2 start main.js --exp-backoff-restart-delay=100 && pm2 logs
