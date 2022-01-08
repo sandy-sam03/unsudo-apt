e9456032207989c026117d40e71d0b7e369cb3ac
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-01-08 18:12:56 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -3,7 +3,7 @@ function handler(m) {
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
   m.reply('Use .bot to msg owner')
   this.sendContact(m.chat, '919462541580', 'Rishabh-Modi', m)
-  this.sendContact(m.chat, '918209611657', 'Rishabh-Modi', m)
+  this.sendContact(m.chat, '', 'Rishabh-Modi', m)
 }
 handler.help = ['owner/creator']
 handler.tags = ['info']
