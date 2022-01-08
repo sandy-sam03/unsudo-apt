da45b419bdbe659726415c2e9ec92634739941cc
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-01-08 17:50:29 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,6 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
+  m.reply('Use .bot to msg owner')
   this.sendContact(m.chat, '919462541580', 'Rishabh-Modi', m)
   this.sendContact(m.chat, '918209611657', 'Rishabh-Modi', m)
 }
