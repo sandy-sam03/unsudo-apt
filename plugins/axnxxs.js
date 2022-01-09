cf8637db2c3e0fef3791f52d071cf3f954da4ef5
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-01-09 15:31:24 +0530

    Update axnxxs.js

--- plugins/axnxxs.js
+++ plugins/axnxxs.js
@@ -1,28 +1,33 @@
 let fetch = require("node-fetch")
 let axios = require("axios")
+let kntl = require("../src/kntl.json")
 let handler = async (m, { conn, text }) => {
-  let chat = global.db.data.chats[m.chat]
-      if (!text) return m.reply("sahi se likh bc")
+  let api = (kntl.lolkey)
+  let chat = global.DATABASE.data.chats[m.chat]
+   if (chat.nsfw) { 
+      if (!text) return m.reply("kuch likh")
       await m.reply(global.wait)
      try {
-      let res = await axios.get(`https://api.lolhuman.xyz/api/xnxxsearch?apikey=1cb304dc7ca35c53cee66a60&query=japanese`)
+      let res = await axios.get(`https://api.lolhuman.xyz/api/xnxxsearch?apikey=1cb304dc7ca35c53cee66a60&query=${text}`)
       let json = res.data
       //let ress = json.result
-      let hsl = `*MUTH MOVEMENT*\n\n`
+      let hsl = `*XNXX SEARCH*\n\n`
       for (let i = 0; i < json.result.length; i++) {
-hsl += `*Title:* ${json.result[i].title}\n`
-hsl += `*Link:*\n${json.result[i].link}\n\n`
-hsl += `*Duration:* ${json.result[i].duration}\n`
-hsl += `*Views:* ${json.result[i].views}\n`
-hsl += `*Rating:* ${json.result[i].rating}\n`
+           hsl += `*Title:* ${json.result[i].title}\n`
+           hsl += `*Views:* ${json.result[i].views}\n`
+           hsl += `*Duration:* ${json.result[i].duration}\n`
+           hsl += `*Uploader:* ${json.result[i].uploader}\n`
+           hsl += `*Link:*\n${json.result[i].link}\n\n`
          }
-           hsl += '*© Araaa•BOT*'
+           hsl += '*SGDC-BOT*'
         conn.reply(m.chat, hsl, m)
     }catch(e){
         m.reply("ERROR")
         console.log(e)
+     }
+    } else {
+       m.reply('```Perlu Mengaktifkan Mode NSFW```')
     }
    }
-handler.help = ['xhamstersearch <milf>']
-handler.command = /^(xhamster|xhamstersearch|xs)$/
+handler.command = /^(xnxxsearch|searchxnxx|xnxx)$/
 module.exports = handler
