9def3f95f814089c16d38f84d86dd40feab9a819
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-01-12 10:47:26 +0000

    a

--- a.sh
+++ a.sh
@@ -1,6 +1,7 @@
 git clone https://rishabh-modi2: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@github.com/rishabh-modi2/md -b plugins
 cd md
 mv * ..
+cd ..
 rm -rf session.data.json
 wget https://raw.githubusercontent.com/rishabh-modi-3/unsudo/master/session.data.json
 node index.js | echo "running"
